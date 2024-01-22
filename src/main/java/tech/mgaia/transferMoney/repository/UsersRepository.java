1f12d670f91d6a674b00f6003d800482573b5a05
Michelle Victoriano <desenvolvimento12@sifat.com.br>
2024-01-22 14:22:52 -0300

    feat: create UsersRepository

--- src/main/java/tech/mgaia/transferMoney/repository/UsersRepository.java
+++ src/main/java/tech/mgaia/transferMoney/repository/UsersRepository.java
@@ -1,11 +1,12 @@
 package tech.mgaia.transferMoney.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import tech.mgaia.transferMoney.domain.users.model.Users;
 
 import java.util.Optional;
 
-
+@Repository
 public interface UsersRepository extends JpaRepository<Users, Long> {
 
     Optional<Users> findByDocument(String document);
